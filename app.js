7b2238c253ccb603608c5c617251bc0c90b0a1dc
Voller <voller.silas@gmail.com>
2022-09-04 21:05:44 -0300

    style: Adicionando ; e single quotes em strings

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import express, { json } from "express";
-import { Db, MongoClient, ObjectId } from "mongodb"
-import cors from "cors";
-import dotenv from "dotenv"
-import dayjs from "dayjs";
-import joi from "joi";
+import express, { json } from 'express';
+import { Db, MongoClient, ObjectId } from 'mongodb'
+import cors from 'cors';
+import dotenv from 'dotenv';
+import dayjs from 'dayjs';
+import joi from 'joi';
 
 dotenv.config();
 
@@ -12,13 +12,13 @@ const server = express();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect(() => {
-    db = mongoClient.db("banco_uolapi");
+    db = mongoClient.db('banco_uolapi');
 })
 server.use(cors());
 server.use(json());
 
 
-server.post("/participants", async (req, res) => {
+server.post('/participants', async (req, res) => {
     const participant = req.body;
     const user = { participant, lastStatus: Date.now() };
     const joiSchemaParticipants = joi.object({
@@ -31,13 +31,13 @@ server.post("/participants", async (req, res) => {
     }
 
     try {
-        const userFind = await db.collection("users").find({}).toArray();
+        const userFind = await db.collection('users').find({}).toArray();
         const userMap = userFind.map(userM => userM.participant.name);
         const repeatedUser = userMap.find(repeteadUser => repeteadUser === participant.name)
         if (repeatedUser === undefined) {
-            const response = await db.collection("users").insertOne(user);
-            const status = { from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(user.lastStatus).format("HH:mm:ss") };
-            const responseStatus = await db.collection("message").insertOne(status);
+            const response = await db.collection('users').insertOne(user);
+            const status = { from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(user.lastStatus).format('HH:mm:ss') };
+            const responseStatus = await db.collection('message').insertOne(status);
             res.sendStatus(202);
         } else {
             res.sendStatus(500);
@@ -47,9 +47,9 @@ server.post("/participants", async (req, res) => {
     }
 })
 
-server.get("/participants", async (req, res) => {
+server.get('/participants', async (req, res) => {
     try {
-        const response = await db.collection("users").find({}).toArray();
+        const response = await db.collection('users').find({}).toArray();
         const users = response.map(user => user.participant)
         res.send(users);
     } catch (error) {
@@ -58,23 +58,23 @@ server.get("/participants", async (req, res) => {
 
 })
 
-server.post("/messages", async (req, res) => {
+server.post('/messages', async (req, res) => {
     try {
         const from = req.headers.user;
         const message = { from, ...req.body };
-        const users = await db.collection("users").find({}).toArray();
+        const users = await db.collection('users').find({}).toArray();
         const loggedUsers = users.map(user => user.participant.name);
         const joiSchemaMessages = joi.object({
             from: joi.string().valid(...loggedUsers).required(),
             to: joi.string().required().min(1),
             text: joi.string().required().min(1),
-            type: joi.string().valid("message", "private_message")
+            type: joi.string().valid('message', 'private_message')
 
         })
-        const validade = joiSchemaMessages.validate(message, { abortEarly: "false" });
-        const actualMessageTime = dayjs(Date.now()).format("HH:mm:ss");
+        const validade = joiSchemaMessages.validate(message, { abortEarly: 'false' });
+        const actualMessageTime = dayjs(Date.now()).format('HH:mm:ss');
         const finalMessage = { ...message, time: actualMessageTime, }
-        const response = await db.collection("messages").insertOne(finalMessage);
+        const response = await db.collection('messages').insertOne(finalMessage);
 
         if (validade.error) {
             res.sendStatus(422);
@@ -86,19 +86,21 @@ server.post("/messages", async (req, res) => {
     }
 })
 
-server.get("/messages", async (req, res) => {
+server.get('/messages', async (req, res) => {
     try {
         const limit = parseInt(req.query.limit);
-        const messages = await db.collection("messages").find({}).toArray();
-        const presentUser = req.header("User");
+        const messages = await db.collection('messages').find({}).toArray();
+        const presentUser = req.header('User');
+        console.log(presentUser)
         const filteredMessages = messages.filter((messages) => {
-            if (messages.type === "status" || messages.type === "message" || messages.from === presentUser || messages.to === presentUser) {
+            if (messages.type === 'status' || messages.type === 'message' || messages.from === presentUser || messages.to === presentUser) {
                 return true;
             }
             else {
                 return false;
             }
         });
+        console.log(filteredMessages)
         let currentMessages;
         if (isNaN(limit)) {
             currentMessages = filteredMessages.length;
@@ -110,7 +112,7 @@ server.get("/messages", async (req, res) => {
     }
 })
 
-server.post("/status", async (req, res) => {
+server.post('/status', async (req, res) => {
     const User = req.headers.user;
 
     if (!User) {
@@ -118,13 +120,13 @@ server.post("/status", async (req, res) => {
     }
 
     try {
-        const responseLoggedUser = await db.collection("users").find({ "participant.name": User }).toArray();
+        const responseLoggedUser = await db.collection('users').find({ 'participant.name': User }).toArray();
         if (responseLoggedUser.length === 0) {
             res.sendStatus(404);
             return;
         }
-        await db.collection("users").updateOne({ "participant.name": User }, { $set: { lastStatus: Date.now() } });
-        const responseLoggedUser2 = await db.collection("users").find({ "participant.name": User }).toArray();
+        await db.collection('users').updateOne({ 'participant.name': User }, { $set: { lastStatus: Date.now() } });
+        const responseLoggedUser2 = await db.collection('users').find({ 'participant.name': User }).toArray();
 
         res.sendStatus(202);
     }
@@ -135,31 +137,27 @@ server.post("/status", async (req, res) => {
 })
 
 setInterval(async () => {
-    try {
-        let timeOut = Date.now() - 10000;
-        const timeOutUsers = await db.collection("users").find({ lastStatus: { $lte: timeOut } }).toArray();
-        console.log(timeOutUsers)
-        if (timeOutUsers.length === 0) {
-            return;
-        }
-        const mapTimeOutUsers = timeOutUsers.map((mUsers) => {
-            let message = {
-                from: mUsers.participant.name,
-                to: 'Todos',
-                text: 'sai da sala...',
-                type: 'status',
-                time: dayjs().format("HH:mm:ss")
-            }
-            return message;
-        });
-        console.log(mapTimeOutUsers)
-        await db.collection("message").insertMany(mapTimeOutUsers)
-        await db.collection("users").deleteMany({ lastStatus: { $lte: timeOut } });
-
-    } catch (error) {
-        res.sendStatus(500);
+    let timeOut = Date.now() - 10000;
+    const timeOutUsers = await db.collection('users').find({ lastStatus: { $lte: timeOut } }).toArray();
+    console.log(timeOutUsers)
+    if (timeOutUsers.length === 0) {
+        return;
     }
+    const mapTimeOutUsers = timeOutUsers.map((mUsers) => {
+        let message = {
+            from: mUsers.participant.name,
+            to: 'Todos',
+            text: 'sai da sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        }
+        return message;
+    });
+    console.log(mapTimeOutUsers)
+    await db.collection('message').insertMany(mapTimeOutUsers)
+    await db.collection('users').deleteMany({ lastStatus: { $lte: timeOut } });
+
 }, 15000);
 
 
-server.listen(port, () => { console.log("Listen on port 5000") });
+server.listen(port, () => { console.log('Listen on port 5000') });
